a41dbfe122542564e52436c6b2d003a3c9fbb31e
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2011-12-17 12:56:47 -0600

    Creado el proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaavi::Application.config.secret_token = '899a63ae16cd572d06855a9f78ba6b2072c7a65e56db2a78d83cd093596aa9d80ea820780d0201362856ba39ae1f958f642224d511db8f8876b822a2a93ba977'
