409f0264ca9ac9b20e98a41cf649280f0e83fab7
Jorge García <jorge.em223@gmail.com>
2012-08-13 00:00:26 -0500

    agregadas pruebas del decorator, se dejaron pendientes algunas que no se como hacer

--- spec/decorators/palabra_decorator_spec.rb
+++ spec/decorators/palabra_decorator_spec.rb
@@ -1,4 +1,40 @@
 require 'spec_helper'
 
 describe PalabraDecorator do
+
+  before(:each) do
+    @palabra_con_traducciones = FactoryGirl.create(:palabra_con_traduccion)
+    @palabra = PalabraDecorator.find(@palabra_con_traducciones.id)
+  end
+
+  it 'should return empty json file' do
+    pending
+    palabra_vacia = FactoryGirl.create(:palabra)
+    palabra_vacia.significados_json.should == 'null'
+  end
+
+  it 'should return an json file with data' do
+    @palabra.significados_json.should_not == 'null'
+  end
+
+  it 'should return test1, test2, test3' do
+    pending
+    @palabra.significados_id_comma.should == 'test1, test2, test3'
+  end
+
+  it 'should relacionar_variante' do
+    pending
+    variante = "algo"
+    @palabra.relacionar_variante(variante).should == nil
+  end
+
+  it 'should not relacionar_variante' do
+    pending
+    @palabra.relacionar_variante(variante).should_not == nil
+  end
+
+  it 'should return a Palabra model' do
+    nombre = @palabra_con_traducciones.palabra
+    @palabra.palabra.should == nombre
+  end
 end
